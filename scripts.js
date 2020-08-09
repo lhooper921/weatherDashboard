91aa139ff26f791120ca0dd449f7dc046593e1cf
Lauren Hooper <lluther921@gmail.com>
2020-08-08 18:24:11 -0700

    Search for current forecast is working plus icons!

--- scripts.js
+++ scripts.js
@@ -1,15 +1,69 @@
 
+let currentDate = moment().format("l");
+
+$("#dateDiv").text(currentDate);
 
 
 
 
-
-
-
-//  click event for save button
+//  Search button click event
 $(".searchBtn").on('click',function(event){
     event.preventDefault();
    var inputText = $(event.target).siblings("input")
    console.log(inputText.val());
+   let cityName = inputText.val();
    localStorage.setItem(inputText.attr("id"),inputText.val())
+//    Add text to city name div
+ $("#cityDiv").text(inputText.val());
+
+
+
+// Current weather
+ var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=90f207048ae63b6cb40d90f49ace5dfc"
+ $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+   
+     .then(function(response) {
+console.log(response)
+    // Define variables
+          var currentTemp = response.main.temp;
+          var currentHumidity = response.main.humidity;
+          var currentWindSpeed = response.wind.speed;
+          var cityLat=response.coord.lat;
+          var cityLon=response.coord.lon;
+        
+    // Insert variables into corresponding divs
+          $("#currentTempDiv").text("Temperature: " + currentTemp); 
+          $("#currentHumidityDiv").text("Humidity: " + currentHumidity); 
+          $("#currentWindDiv").text("Wind Speed: " + currentWindSpeed); 
+        //   Find current weather icon
+        var currentIcon = response.weather[0].icon
+        var queryICONurl = "http://openweathermap.org/img/wn/" + currentIcon + "@2x.png"
+
+        // Empty the div before adding new icon
+        $("#iconDiv").empty();
+        // Display on page
+        $(`<img src='${queryICONurl}'>`).appendTo('#iconDiv');
+
+        // To find UV index, coordinates must be used from previous query
+          var queryUVurl="http://api.openweathermap.org/data/2.5/uvi?appid=90f207048ae63b6cb40d90f49ace5dfc&lat=" + cityLat + "&lon=" + cityLon
+          $.ajax({
+            url: queryUVurl,
+            method: "GET"
+          })
+          .then(function(response) {
+            console.log(response);
+
+            var currentUV = response.value;
+            $("#currentUVDiv").text("UV Index: " + currentUV); 
+          })
+
+       
+      
+          
 })
+     })
+
+
